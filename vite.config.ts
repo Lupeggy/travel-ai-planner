54dcf2d2e26773af1ec0cf38445fe4c1b034e372
Peggy Lu <lupeggy0608@gmail.com>
2025-07-26 16:17:14 +1200

    route setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  ssr: {
+    noExternal: ["@syncfusion/ej2-base", "@syncfusion/ej2-react-buttons", "@syncfusion/ej2-react-charts", "@syncfusion/ej2-react-dropdowns", "@syncfusion/ej2-react-grids", "@syncfusion/ej2-react-maps", "@syncfusion/ej2-react-navigations", "@syncfusion/ej2-react-splitbuttons", "@syncfusion/ej2-react-dropdowns"],
+  }
 });
